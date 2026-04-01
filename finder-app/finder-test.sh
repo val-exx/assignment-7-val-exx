5943d6ca4f758444b61c162718a33d484e3d7d0a
val-exx <stabellinivalentina@gmail.com>
2026-04-01 14:35:15 +0200

    Completed Assignment 2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -56,7 +56,7 @@ make
 
 for i in $( seq 1 $NUMFILES)
 do
-	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
