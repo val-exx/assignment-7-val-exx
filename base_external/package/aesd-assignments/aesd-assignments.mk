868a557ca5021b008587e0859b89199824c08da3
val-exx <stabellinivalentina@gmail.com>
2026-05-02 23:25:23 +0200

    Update aesd assignments package version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'eff749b'
+AESD_ASSIGNMENTS_VERSION = '3c0070d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
