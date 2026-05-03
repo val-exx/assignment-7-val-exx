094a827013721204438063814dd586d10f745fd9
val-exx <stabellinivalentina@gmail.com>
2026-05-03 15:56:05 +0200

    Updated correct github repo on aesd-assignments.mk package

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '3c0070d'
+AESD_ASSIGNMENTS_VERSION = '5b571fb'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:val-exx/assignment-5-val-exx.git'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:val-exx/assignment-3-and-later-val-exx.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
