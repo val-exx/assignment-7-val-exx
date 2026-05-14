50671f20be831bb01697c2b9e8d0157f2891f114
val-exx <stabellinivalentina@gmail.com>
2026-05-14 14:59:32 +0200

    added username

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, val-exx\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, val-exx\n");
 }
 
 module_init(hello_init);
